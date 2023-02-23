812a3171fa793a6d23a7fc0666f21fc00ab1d4e6
Poornesh-M-D <70961243+Poornesh-M-D@users.noreply.github.com>
2023-02-23 09:35:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+const pageNumberH3 = document.getElementById("pageNumber");
+var pageNumber = 1;
+async function fetchData(pageNumber) {
+	const response = await fetch(``);
+	const data = await response.json();
+	console.log("DATA",data);
+}
+
+function loadNext(){
+	pageNumber++;
+	if(pageNumber==2)
+	{
+		document.getElementById("load_prev").disabled=false;
+	}
+	pageNumberH3.innerText = pageNumber;
+	fetchData(pageNumber);
+}
+
+function loadPrev(){
+	if(pageNumber==2)
+	{
+		document.getElementById("load_prev").disabled=true;
+	}else
+	{
+		document.getElementById("load_prev").disabled=false;
+	}
+	pageNumber--;
+	pageNumberH3.innerText = pageNumber;
+	fetchData(pageNumber);
+}
+	fetchData(1);
